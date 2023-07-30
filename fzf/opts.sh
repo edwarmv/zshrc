55341a57efe7101da5cfb261efe1fca8298ebcb9
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2023-07-30 12:36:11 -0400

    first commit

---
+++ fzf/opts.sh
@@ -0,0 +1,38 @@
+FZF_DEFAULT_OPTS="
+--bind=alt-j:preview-down,alt-k:preview-up,alt-d:preview-page-down,alt-u:preview-page-up,change:first,alt-a:select-all,alt-A:deselect-all,alt-p:toggle-preview
+--preview-window noborder
+--layout=reverse
+"
+# export FZF_DEFAULT_OPTS="
+# --bind=alt-j:preview-down,alt-k:preview-up,alt-d:preview-page-down,alt-u:preview-page-up,change:first,alt-a:select-all,alt-A:deselect-all,alt-p:toggle-preview
+# --preview-window noborder
+# --layout=reverse
+# --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796
+# --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6
+# --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#chttps://medium.com/codex/how-and-why-you-should-split-your-bashrc-or-zshrc-files-285e5cc3c8436a0f6,hl+:#ed8796
+# --color=separator:#8aadf4
+# --color=border:#8aadf4
+# "
+
+# gruvbox_material
+# export FZF_DEFAULT_OPTS="
+# --bind=alt-j:preview-down,alt-k:preview-up,alt-d:preview-page-down,alt-u:preview-page-up,change:first,alt-a:select-all,alt-A:deselect-all,alt-p:toggle-preview
+# --preview-window noborder
+# --layout=reverse
+# --color=fg:#928374,bg:-1,hl:#7daea3
+# --color=fg+:#928374,bg+:#3c3836,hl+:#7daea3
+# --color=info:#d8a657,prompt:#d3869b,pointer:#d3869b
+# --color=marker:#a9b665,spinner:#d3869b,header:#a9b665,preview-bg:-1
+# --color=border:#928374
+# --color=separator:#665c54
+# "
+
+# tokyonight_storm
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
+--color=fg:#565f89,bg:#24283b,hl:#2ac3de
+--color=fg+:#565f89,bg+:#292e42,hl+:#2ac3de
+--color=info:#7aa2f7,prompt:#7aa2f7,pointer:#db4b4b
+--color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a
+--color=border:#2ac3de
+--color=separator:#2ac3de
+"
