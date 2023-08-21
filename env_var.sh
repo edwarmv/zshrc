bef679590fd0ef971f77c5796d770b0807b7eb44
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-08-21 15:33:18 -0400

    feat(nnn): create

--- env_var.sh
+++ env_var.sh
@@ -1,9 +1,12 @@
 export PATH=":$HOME/.local/bin:$PATH"
 export PATH=":$GOPATH/bin:$PATH"
+export PATH=":/usr/share/git/diff-highlight:$PATH"
 export TERMINFO_DIRS=":$HOME/.local/share/terminfo"
 export EXA_COLORS="reset"
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export LS_COLORS="di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
-export EDITOR="nvim"
 export XDG_CONFIG_HOME=$HOME/.config
 export ZSH_TMUX_CONFIG=$HOME/.config/tmux/tmux.conf
+export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
+export VISUAL="nvim"
+export EDITOR="nvim"
