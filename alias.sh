fd4e4747916a8f54eb3e4fda51dc4a26de54c1c5
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-06-06 10:45:05 -0400

    add alias

--- alias.sh
+++ alias.sh
@@ -1,2 +1,5 @@
 alias vi="nvim"
 alias install-kitty-nightly="curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin installer=nightly"
+alias lc="lfcd"
+alias lg="lazygit"
+alias ld="lazydocker"
