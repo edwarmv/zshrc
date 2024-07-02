5ee99c6883eee53c6b08950ea2104ba0b1282a66
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-07-02 16:47:37 -0400

    feat: add fzf-git

--- plugins/zvm_vi.sh
+++ plugins/zvm_vi.sh
@@ -5,3 +5,22 @@
 # tokyonight_storm
 ZVM_VI_HIGHLIGHT_FOREGROUND=#c0caf5
 ZVM_VI_HIGHLIGHT_BACKGROUND=#2e3c64
+
+# REF https://github.com/junegunn/fzf-git.sh/pull/46/files
+function zvm_after_init() {
+  zvm_bindkey viins "^P" up-line-or-beginning-search
+  zvm_bindkey viins "^N" down-line-or-beginning-search
+  for o in files branches tags remotes hashes stashes lreflogs each_ref; do
+    eval "zvm_bindkey viins '^g^${o[1]}' fzf-git-$o-widget"
+    eval "zvm_bindkey viins '^g${o[1]}' fzf-git-$o-widget"
+  done
+}
+# Set key bindings for zsh-vi-mode normal and visual modes.
+function zvm_after_lazy_keybindings() {
+  for o in files branches tags remotes hashes stashes lreflogs each_ref; do
+    eval "zvm_bindkey vicmd '^g^${o[1]}' fzf-git-$o-widget"
+    eval "zvm_bindkey vicmd '^g${o[1]}' fzf-git-$o-widget"
+    eval "zvm_bindkey visual '^g^${o[1]}' fzf-git-$o-widget"
+    eval "zvm_bindkey visual '^g${o[1]}' fzf-git-$o-widget"
+  done
+}
