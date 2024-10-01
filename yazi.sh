3f608fbc8e65bbb1aa600bb7da24c40a6d1daa1b
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-09-30 22:32:33 -0400

    feat: add yazi

---
+++ yazi.sh
@@ -0,0 +1,8 @@
+function yc() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
