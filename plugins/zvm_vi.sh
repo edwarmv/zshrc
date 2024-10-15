7080b81b54590803c15f5f005d6b886afd4982f7
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2024-10-15 09:28:59 -0400

    update zvm_vi

--- plugins/zvm_vi.sh
+++ plugins/zvm_vi.sh
@@ -10,6 +10,9 @@ ZVM_VI_HIGHLIGHT_BACKGROUND=#2e3c64
 function zvm_after_init() {
   zvm_bindkey viins "^P" up-line-or-beginning-search
   zvm_bindkey viins "^N" down-line-or-beginning-search
+  zvm_bindkey viins '^R' fzf-history-widget
+  zvm_bindkey vicmd '^R' redo
+  zvm_bindkey viins '\ec' fzf-cd-widget
   for o in files branches tags remotes hashes stashes lreflogs each_ref; do
     eval "zvm_bindkey viins '^g^${o[1]}' fzf-git-$o-widget"
     eval "zvm_bindkey viins '^g${o[1]}' fzf-git-$o-widget"
